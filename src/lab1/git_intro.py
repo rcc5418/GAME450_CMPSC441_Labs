b33a6db3ed9a1f1e279bc98e2b4d81a207875bdf
Pulin Agrawal <pulinagrawal@users.noreply.github.com>
2024-01-16 10:24:16 -0500

    Update git_intro.py

--- src/lab1/git_intro.py
+++ src/lab1/git_intro.py
@@ -2,11 +2,10 @@
 
 In this lab, you will learn how to use git and github to manage your code.
 
-Fork this repository to your own github account.
 Then clone the repository to your local machine.
 Edit this file to: 
     Print 'Hello <Your Name>' to the console.
 Commit your changes to your local repository.
-Push your changes to your forked repository on github.
+Push your changes to a new repository in github.
 '''
 print("hello")
